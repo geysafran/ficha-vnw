806e93a6819fad79720804920f5f4a3b687866fd
vai na web <vainawebcpx@gmail.com>
2019-11-01 12:28:21 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,61 @@
+let ficha = [
+{
+    nome: 'Rhuan',
+    idade: '20 anos',
+    polo: 'Pzr'
+   },
+   {
+    nome: 'Jorge Luis',
+    idade:'58 anos',
+    polo: 'cpx'
+   },
+   {
+    nome: 'maicon',
+    idade: '23 anos',
+    polo: 'cpx'
+   },
+   {
+    nome: 'Caroline',
+    idade: '19 anos',
+    polo: 'pzr'
+   },
+   {
+    nome: 'Estefania',
+    idade: '22 anos',
+    polo: 'pzr'
+   },
+   {
+    nome: 'Victor',
+    idade: '24 anos',
+    polo: 'cpx'
+   },
+   {
+    nome: 'Zaira',
+    idade: '24 anos',
+    polo: 'pzr'
+   },
+   {
+    nome: 'Cleiton',
+    idade: '19 anos',
+    polo: 'cpx'
+   },
+   {
+    nome: 'Rhaysssa',
+    idade: '21 anos',
+    polo: 'cpx'
+   },
+   {
+    nome: 'Luan',
+    idade: '18 anos',
+    polo: 'cpx'
+   },
+   {
+    nome: 'Arthur',
+    idade: '24 anos',
+    polo: 'cpx'
+   },
+ 
+];
+
+let parara = ficha.map(nome => nome.nome);
+document.write(parara);
